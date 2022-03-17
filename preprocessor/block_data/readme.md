ee2e9819923cf0bf42d976b089ece6d033e4864a
Ben Murphy <ben@pop-os.localdomain>
2022-03-17 15:35:31 +0100

    blockdatareadme

---
+++ preprocessor/block_data/readme.md
@@ -0,0 +1,2 @@
+# block #4710599 mined at 420 
+# block #7229127 first block parainherent
--- preprocessor/db/db_test.py
+++ preprocessor/db/db_test.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     query = """
         query {
             blocks {
-                number
+                header
             }
         }
     """
